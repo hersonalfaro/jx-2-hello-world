bf52bc077a981bdeb63980e4838d8daf9b9ec4dc
Herson Alfaro <herson.alfaro@gorillalogic.com>
2019-02-13 13:35:01 -0600

    Initial import

---
+++ src/main/java/com/hersonalfaro/jx/jx2helloworld/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.hersonalfaro.jx.jx2helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
